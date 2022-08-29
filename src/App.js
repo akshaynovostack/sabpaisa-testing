90e37f9c40402e7b4b4d550c3c7bba26260bc656
akshay <akshaycyber96@gmail.com>
2022-08-29 18:20:30 +0530

    updated error resolved

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { PaymentModal } from 'pg-test-project';
+import { Button } from "reactstrap";
+import { useState } from 'react';
 function App() {
+  const [isOpen, openPg] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button onClick={() => openPg(true)}>Open Payment Gateway</Button>
+
+        <PaymentModal isOpen={isOpen} label={"testing"} />
       </header>
     </div>
   );
